f211391ab488c2c3526df3c4ded23016a7909b9b
BB <92384891+BatiB@users.noreply.github.com>
2021-10-17 18:32:55 +0200

    Update AppCoinG_v0.py

--- AppCoinG_v0.py
+++ AppCoinG_v0.py
@@ -17,18 +17,23 @@ st.markdown('''# **CryptoMarket App**
 A simple cryptocurrency price app pulling price data from *CoinGecko*.
 ''')
 
-st.header('** TOP 100 CRYPTO **')
+st.header('** Top 250 Crypto by Market Cap **')
+st.markdown('''Top 5 Crypto by MarketCap''')
 
 
-st.markdown('''Top 5 Crypto per MarketCap''')
 col1, col2, col3, col4, col5 = st.columns(5)
+
 #col1.metric(Col1['name'], Col1['current_price', Col1['price_change_percentage_24h']])
 
 name = 'BTCUSD'
 price = '60000'
 perc = '0.05'
 
-col1.metric(name, price , perc)
+col1.metric(name, price, perc)
+col2.metric(name, price, perc)
+col3.metric(name, price, perc)
+col4.metric(name, price, perc)
+col5.metric(name, price, perc)
 
 #col2.metric(MarketData.loc[[1], ['name', 'current_price', 'price_change_percentage_24h']])
 #col3.metric(MarketData.loc[[2], ['name', 'current_price', 'price_change_percentage_24h']])
