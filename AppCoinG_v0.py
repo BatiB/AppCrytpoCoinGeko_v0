0f7dd8310801a50fd7dfd8c37d00f69336bd4314
BB <92384891+BatiB@users.noreply.github.com>
2021-10-17 18:48:35 +0200

    Update AppCoinG_v0.py

--- AppCoinG_v0.py
+++ AppCoinG_v0.py
@@ -10,8 +10,6 @@ MarketData = cg.get_coins_markets(vs_currency='usd', per_page=250, page=1)
 MarketData = pd.DataFrame(MarketData)
 #print(MarketData)
 
-Col1 = MarketData.loc[[0], ['name', 'current_price', 'price_change_percentage_24h']]
-#print(Col1)
 
 st.markdown('''# **CryptoMarket App**
 A simple cryptocurrency price app pulling price data from *CoinGecko*.
@@ -20,25 +18,33 @@ A simple cryptocurrency price app pulling price data from *CoinGecko*.
 st.header('** Top 250 Crypto by Market Cap **')
 st.markdown('''Top 5 Crypto by MarketCap''')
 
-
-col1, col2, col3, col4, col5 = st.columns(5)
-
-#col1.metric(Col1['name'], Col1['current_price', Col1['price_change_percentage_24h']])
+st.header('** Top 250 Crypto by Market Cap **')
 
 name = 'BTCUSD'
-price = 61234.56
+price = 60000
 perc = f'{0.05}%'
 
+col1, col2, col3, col4, col5 = st.columns(5)
+st.markdown('''Top 5 Crypto by Market Captalisation''')
 col1.metric(name, price, perc)
 col2.metric(name, price, perc)
 col3.metric(name, price, perc)
 col4.metric(name, price, perc)
 col5.metric(name, price, perc)
 
-#col2.metric(MarketData.loc[[1], ['name', 'current_price', 'price_change_percentage_24h']])
-#col3.metric(MarketData.loc[[2], ['name', 'current_price', 'price_change_percentage_24h']])
-#col4.metric(MarketData.loc[[3], ['name', 'current_price', 'price_change_percentage_24h']])
-#col5.metric(MarketData.loc[[4], ['name', 'current_price', 'price_change_percentage_24h']])
+st.markdown('''Top 5 performers (24h)''')
+col1.metric(name, price, perc)
+col2.metric(name, price, perc)
+col3.metric(name, price, perc)
+col4.metric(name, price, perc)
+col5.metric(name, price, perc)
+
+st.markdown('''Worst 5 performers (24h)''')
+col1.metric(name, price, perc)
+col2.metric(name, price, perc)
+col3.metric(name, price, perc)
+col4.metric(name, price, perc)
+col5.metric(name, price, perc)
 
 st.header('**All Price**')
 st.dataframe(MarketData)
