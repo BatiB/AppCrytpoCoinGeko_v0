2d9a431ba826719bc4775a4d78a36a6ad902f3c5
BB <92384891+BatiB@users.noreply.github.com>
2021-10-18 15:25:58 +0200

    Update AppCoinG_v0.py

--- AppCoinG_v0.py
+++ AppCoinG_v0.py
@@ -73,7 +73,7 @@ for i in range(0, 10):
     cols = st.columns(4)
     cols[0].write(f'{rank1[i]}')
     cols[1].write(name1[i])
-    cols[2].write(price1[i])
+    cols[2].write(f'{price1[i]}')
     cols[3].write(perc1[i])
 
 
